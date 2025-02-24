25d995e84552416879c207b70047c1862f410fb9
Ivan-Kosty <kivan6386@gmail.com>
2025-02-24 18:31:06 +0300

    Add project files.

---
+++ L43Solution/test03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int* pointer = NULL;
+	pointer = new int;
+
+	*pointer = 10;
+	cout << sizeof(pointer) << " - " << pointer << endl;
+	cout << pointer << endl;
+	cout << *pointer << endl;
+
+	delete pointer;
+
+	return 0;
+}
